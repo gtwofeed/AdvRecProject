3f6d8c2f962d9e85c52a016b2c9e0edcf5592825
Ruslan <Ruslan@PC59>
2023-09-15 11:41:51 +0300

    отображаем наличие детей

---
+++ GrpcWpfClient/Views/BoollToStringFoStartPageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GrpcWpfClient.Views
+{
+    public class BoollToStringFoStartPageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true) return new string("Родитель");
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value is not null) return true;
+            else return false;
+        }
+    }
+}
